ca27054a1cc2f62e0b39c234e6b672b70320e2b3
alejandrorvilla <alejandrorvilla07@gmail.com>
2022-10-30 22:46:21 -0500

    Update images

--- src/services/product/data.ts
+++ src/services/product/data.ts
@@ -5,7 +5,7 @@ export const data: Array<IProduct> = [
     id: 1,
     name: "De Todito",
     image:
-      "https://licoresbrisasdejuanchito.com/wp-content/uploads/2021/09/DE-TODITO-NATURAL-X-400G.png",
+      "https://drive.google.com/uc?export=view&id=1yND2Zsm-9tYLVxaySkoEMb58bDNT1A42",
     price: 12000,
     stock: 10,
     description:
@@ -15,7 +15,7 @@ export const data: Array<IProduct> = [
     id: 2,
     name: "Ferrero Rocher",
     image:
-      "https://licoresbrisasdejuanchito.com/wp-content/uploads/2021/09/FERRERO-ROCHER-X-100G.png",
+      "https://drive.google.com/uc?export=view&id=1HM-34mgS0dVjU1h1Gc9yko39RuOLOGWJ",
     price: 30000,
     stock: 8,
     description:
@@ -25,7 +25,7 @@ export const data: Array<IProduct> = [
     id: 3,
     name: "Saviloe",
     image:
-      "https://licoresbrisasdejuanchito.com/wp-content/uploads/2021/04/SAVILOE-420ML-600x900.jpg",
+      "https://drive.google.com/uc?export=view&id=1TZVVEh07qytKpkK8YPQpPHzyS-AQD15z",
     price: 3500,
     stock: 5,
     description:
@@ -35,7 +35,7 @@ export const data: Array<IProduct> = [
     id: 4,
     name: "CocaCola 3L",
     image:
-      "https://licoresbrisasdejuanchito.com/wp-content/uploads/2021/04/GASEOSA-COCACOLA-3LTS.png",
+      "https://drive.google.com/uc?export=view&id=18REkNJTEVTuj-HMuIgCNdu6ERQqg4-vJ",
     price: 8000,
     stock: 5,
     description:
@@ -45,7 +45,7 @@ export const data: Array<IProduct> = [
     id: 5,
     name: "Chiclets Adams",
     image:
-      "https://licoresbrisasdejuanchito.com/wp-content/uploads/2021/09/CHICLETS-ADAMS-X-12.png",
+      "https://drive.google.com/uc?export=view&id=1FfCjGFs4rvmTEnbiu8fUN-cSEaX-bICD",
     price: 1250,
     stock: 2,
     description:
@@ -55,7 +55,7 @@ export const data: Array<IProduct> = [
     id: 6,
     name: "Jugo Hit 1L",
     image:
-      "https://licoresbrisasdejuanchito.com/wp-content/uploads/2021/10/HIT-TETRA-PACK-X-1L.png",
+      "https://drive.google.com/uc?export=view&id=1KXJUxgXA0CfVHHKFhzGfG1vEVKfLONjD",
     price: 4000,
     stock: 15,
     description:
@@ -65,7 +65,7 @@ export const data: Array<IProduct> = [
     id: 7,
     name: "Mani Salado",
     image:
-      "https://licoresbrisasdejuanchito.com/wp-content/uploads/2021/09/MANI-ESPEC-MANISALX180GR24.png",
+      "https://drive.google.com/uc?export=view&id=1oh70nDJgorfvfQgR4-dKfdELZIvoI1ZO",
     price: 3500,
     stock: 12,
     description:
@@ -75,7 +75,7 @@ export const data: Array<IProduct> = [
     id: 8,
     name: "De Todito BBQ",
     image:
-      "https://licoresbrisasdejuanchito.com/wp-content/uploads/2021/09/DE-TODITO-X-400G.png",
+      "https://drive.google.com/uc?export=view&id=1h417dgAGZci0s2-Xbxpc1-vwX4PQEroc",
     price: 12000,
     stock: 12,
     description:
@@ -85,7 +85,7 @@ export const data: Array<IProduct> = [
     id: 9,
     name: "Choclitos",
     image:
-      "https://licoresbrisasdejuanchito.com/wp-content/uploads/2021/09/CHOCLITOS-X-180G.png",
+      "https://drive.google.com/uc?export=view&id=1jUzf-1HeR1ogqN1dU0_CMwP_H1bNyDLu",
     price: 3500,
     stock: 8,
     description:
@@ -95,7 +95,7 @@ export const data: Array<IProduct> = [
     id: 10,
     name: "Salchicha Zenu",
     image:
-      "https://licoresbrisasdejuanchito.com/wp-content/uploads/2021/04/SALCHICHA-ZENUX150GRS-600x899.jpg",
+      "https://drive.google.com/uc?export=view&id=1C_T-xdhdCOFTqXud21Rj7uUj93lN-m4F",
     price: 3200,
     stock: 8,
     description:
@@ -105,7 +105,7 @@ export const data: Array<IProduct> = [
     id: 11,
     name: "Manzana Postobon 1.5L",
     image:
-      "https://licoresbrisasdejuanchito.com/wp-content/uploads/2021/04/GASEOSA-POSTOBON-1500.png",
+      "https://drive.google.com/uc?export=view&id=10787C3zKCbM_z0wcSREHfCBAHI13yzSF",
     price: 5000,
     stock: 7,
     description:
